0789d32119c632a8458af8707adc6dd453d5ad68
sripavantejb <sripavantejb@gmail.com>
2025-10-09 16:07:47 +0530

    updated background video padding

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Background() {
   return (
     <>
-      <section className="relative z-0 w-full h-[70vh] md:h-[80vh] overflow-hidden">
+      <section className="relative mt-8 z-0 w-full h-[70vh] md:h-[80vh] overflow-hidden">
         <video
           className="object-cover w-full h-full"
           src="https://res.cloudinary.com/dqataciy5/video/upload/v1758301511/Timeline_1_xhvbls.mov"
